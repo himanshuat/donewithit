a0c6cb5982be5babdd548ddad5560d320d5c54dd
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-14 01:34:56 +0530

    Build image input component

--- App.js
+++ App.js
@@ -1,44 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Button, FlatList, Image } from 'react-native';
+import React, { useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import * as ImagePicker from 'expo-image-picker';
 
 import Screen from './app/components/Screen';
+import ImageInput from './app/components/ImageInput';
 
 export default function App() {
-	const [assets, setAssets] = useState([]);
-
-	const requestPermission = async () => {
-		const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
-		if (!granted) {
-			alert("You need to enable permission to access the library.");
-		}
-	}
-
-	useEffect(() => {
-		requestPermission();
-	}, [])
-
-	const selectImage = async () => {
-		try {
-			const result = await ImagePicker.launchImageLibraryAsync({ allowsMultipleSelection: true });
-			if (!result.canceled) {
-				setAssets(result.assets);
-				console.log(assets);
-			}
-		} catch (error) {
-			console.log("Error reading an image", error);
-		}
-	}
+	const [imageUri, setImageUri] = useState();
 
 	return (
 		<GestureHandlerRootView>
 			<Screen>
-				<Button title='Select Image' onPress={selectImage} />
-				<FlatList
-					data={assets}
-					keyExtractor={item => item.assetId}
-					renderItem={({ item }) => <Image source={{ uri: item.uri }} style={{ width: 200, height: 200 }} />}
+				<ImageInput
+					imageUri={imageUri}
+					onChangeImage={uri => setImageUri(uri)}
 				/>
 			</Screen>
 		</GestureHandlerRootView>
