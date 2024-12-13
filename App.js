bda47dad8703b0d488fce5498a86aaaf16adf27b
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-14 00:00:42 +0530

    Try accessing media library

--- App.js
+++ App.js
@@ -1,12 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Button, FlatList, Image } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import * as ImagePicker from 'expo-image-picker';
 
-import WelcomeScreen from './app/screens/WelcomeScreen';
+import Screen from './app/components/Screen';
 
 export default function App() {
+	const [assets, setAssets] = useState([]);
+
+	const requestPermission = async () => {
+		const { granted } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+		if (!granted) {
+			alert("You need to enable permission to access the library.");
+		}
+	}
+
+	useEffect(() => {
+		requestPermission();
+	}, [])
+
+	const selectImage = async () => {
+		try {
+			const result = await ImagePicker.launchImageLibraryAsync({ allowsMultipleSelection: true });
+			if (!result.canceled) {
+				setAssets(result.assets);
+				console.log(assets);
+			}
+		} catch (error) {
+			console.log("Error reading an image", error);
+		}
+	}
+
 	return (
 		<GestureHandlerRootView>
-			<WelcomeScreen />
+			<Screen>
+				<Button title='Select Image' onPress={selectImage} />
+				<FlatList
+					data={assets}
+					keyExtractor={item => item.assetId}
+					renderItem={({ item }) => <Image source={{ uri: item.uri }} style={{ width: 200, height: 200 }} />}
+				/>
+			</Screen>
 		</GestureHandlerRootView>
 	);
 }
