7483c26782bd35dbcd5874bb4908a8b585e8c648
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-15 00:06:48 +0530

    Improve listing edit screen

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -3,19 +3,21 @@ import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
 import {
-	AppForm,
+	Form,
 	FormField,
 	FormPicker,
 	SubmitButton,
 } from "../components/forms";
 import Screen from "../components/Screen";
 import CategoryPickerItem from "../components/CategoryPickerItem";
+import FormImagePicker from "../components/forms/FormImagePicker";
 
 const validationSchema = Yup.object().shape({
 	title: Yup.string().required().min(1).label("Title"),
 	price: Yup.number().required().min(1).max(10000).label("Price"),
 	description: Yup.string().label("Description"),
 	category: Yup.object().required().nullable().label("Category"),
+	images: Yup.array().min(1, "Please select at least one image"),
 });
 
 const categories = [
@@ -78,16 +80,18 @@ const categories = [
 function ListingEditScreen() {
 	return (
 		<Screen style={styles.container}>
-			<AppForm
+			<Form
 				initialValues={{
 					title: "",
 					price: "",
 					description: "",
 					category: null,
+					images: [],
 				}}
 				onSubmit={(values) => console.log(values)}
 				validationSchema={validationSchema}
 			>
+				<FormImagePicker name="images" />
 				<FormField maxLength={255} name="title" placeholder="Title" />
 				<FormField
 					keyboardType="numeric"
@@ -112,7 +116,7 @@ function ListingEditScreen() {
 					placeholder="Description"
 				/>
 				<SubmitButton title="Post" />
-			</AppForm>
+			</Form>
 		</Screen>
 	);
 }
