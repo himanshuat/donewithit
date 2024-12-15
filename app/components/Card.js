172d997c9bb110403adab30278b3f250f3332345
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-15 15:00:30 +0530

    Build feed navigator

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
+import { View, StyleSheet, Image, TouchableWithoutFeedback } from 'react-native';
 
 import colors from '../config/colors';
 import Text from './Text';
 
-function Card({ title, subTitle, image }) {
+function Card({ title, subTitle, image, onPress }) {
 	return (
-		<View style={styles.card}>
-			<Image style={styles.image} source={image} />
-			<View style={styles.detailsContainer}>
-				<Text style={styles.title}>{title}</Text>
-				<Text style={styles.subTitle}>{subTitle}</Text>
+		<TouchableWithoutFeedback onPress={onPress} >
+			<View style={styles.card}>
+				<Image style={styles.image} source={image} />
+				<View style={styles.detailsContainer}>
+					<Text style={styles.title}>{title}</Text>
+					<Text style={styles.subTitle}>{subTitle}</Text>
+				</View>
 			</View>
-		</View>
+		</TouchableWithoutFeedback>
 	);
 }
 
