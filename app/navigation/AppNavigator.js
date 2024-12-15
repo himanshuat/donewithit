e1d1a27319bb1a82d153613df3fc9e759753107a
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-15 15:48:42 +0530

    Refactor routes

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -6,30 +6,31 @@ import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from './FeedNavigator';
 import AccountNavigator from './AccountNavigator';
 import NewListingButton from './NewListingButton';
+import routes from './routes';
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
 	<Tab.Navigator>
 		<Tab.Screen
-			name="Feed"
+			name={routes.FEED}
 			component={FeedNavigator}
 			options={{
 				tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name='home' color={color} size={size} />
 			}}
 		/>
 		<Tab.Screen
-			name="ListingEdit"
+			name={routes.LISTING_EDIT}
 			component={ListingEditScreen}
 			options={({ navigation }) => ({
 				tabBarButton: () => (
-					<NewListingButton onPress={() => navigation.navigate("ListingEdit")} />
+					<NewListingButton onPress={() => navigation.navigate(routes.LISTING_EDIT)} />
 				),
 				tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name='plus-circle' color={color} size={size} />
 			})}
 		/>
 		<Tab.Screen
-			name="Account"
+			name={routes.ACCOUNT}
 			component={AccountNavigator}
 			options={{
 				tabBarIcon: ({ color, size }) => <MaterialCommunityIcons name='account' color={color} size={size} />
---
+++ app/navigation/routes.js
@@ -0,0 +1,11 @@
+export default Object.freeze({
+    WELCOME: 'Welcome',
+    LOGIN: 'Login',
+    REGISTER: 'Register',
+    ACCOUNT: 'Account',
+    FEED: 'Feed',
+    LISTINGS: 'Listings',
+    LISTING_DETAILS: 'ListingDetails',
+    LISTING_EDIT: 'ListingEdit',
+    MESSAGES: 'Messages',
+})
