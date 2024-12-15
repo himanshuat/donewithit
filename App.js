cdc93f9cf2ed043e18f053d1dffff43dc7c737fa
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-15 14:32:08 +0530

    Build app navigator

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+
 import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
+import AppNavigator from './app/navigation/AppNavigator';
 
 export default function App() {
 	return (
 		<GestureHandlerRootView>
 			<NavigationContainer theme={navigationTheme}>
-				<AuthNavigator />
+				<AppNavigator />
 			</NavigationContainer>
 		</GestureHandlerRootView>
 	);
