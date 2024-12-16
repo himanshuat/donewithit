cc60e5b5d77ad622003657c5e8308d48790aab2e
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-16 18:43:59 +0530

    Use api to fetch listings

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FlatList, StyleSheet } from 'react-native';
 
 import Card from '../components/Card';
 import Screen from '../components/Screen';
 import colors from '../config/colors';
 import routes from '../navigation/routes';
-
-const listings = [
-	{
-		id: 1,
-		title: "Red jacket for sale",
-		price: 100,
-		image: require('../assets/jacket.jpg'),
-	},
-	{
-		id: 2,
-		title: "Couch in great condition",
-		price: 500,
-		image: require('../assets/couch.jpg'),
-	},
-]
+import listingsApi from '../api/listings';
+import AppText from '../components/Text';
+import AppButton from '../components/Button';
+import ActivityIndicator from '../components/ActivityIndicator';
 
 function ListingsScreen({ navigation }) {
+	const [listings, setListings] = useState([]);
+	const [error, setError] = useState(false);
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		loadListings();
+	}, [])
+
+	const loadListings = async () => {
+		setLoading(true);
+		const response = await listingsApi.getListings();
+		setLoading(false);
+
+		if (!response.ok) {
+			setError(true);
+			return;
+		}
+
+		setError(false);
+		setListings(response.data);
+	}
+
 	return (
 		<Screen style={styles.screen}>
+			{error && (
+				<>
+					<AppText>Couldn't retrieve the listings.</AppText>
+					<AppButton title="Retry" onPress={loadListings} />
+				</>
+			)}
+			<ActivityIndicator visible={loading} />
 			<FlatList
 				data={listings}
 				keyExtractor={listing => listing.id.toString()}
@@ -31,7 +49,7 @@ function ListingsScreen({ navigation }) {
 					<Card
 						title={item.title}
 						subTitle={"$" + item.price}
-						image={item.image}
+						imageUrl={item.images[0].url}
 						onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
 					/>
 				)}
