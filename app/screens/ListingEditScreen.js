1142730eb3ffd4169b240044b13d75eb36272aa4
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-17 18:01:42 +0530

    Add progress bar and done animation

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import * as Yup from "yup";
 
@@ -13,6 +13,7 @@ import CategoryPickerItem from "../components/CategoryPickerItem";
 import FormImagePicker from "../components/forms/FormImagePicker";
 import useLocation from "../hooks/useLocation";
 import listingsApi from "../api/listings";
+import UploadScreen from "./UploadScreen";
 
 const validationSchema = Yup.object().shape({
 	title: Yup.string().required().min(1).label("Title"),
@@ -81,17 +82,32 @@ const categories = [
 
 function ListingEditScreen() {
 	const location = useLocation();
+	const [uploadVisible, setUploadVisible] = useState(false);
+	const [progress, setProgress] = useState(0);
 
-	const handleSubmit = async (listing) => {
-		const result = await listingsApi.addListing({ ...listing, location });
+	const handleSubmit = async (listing, { resetForm }) => {
+		setProgress(0);
+		setUploadVisible(true);
+		const result = await listingsApi.addListing(
+			{ ...listing, location },
+			progress => setProgress(progress)
+		);
+
 		if (!result.ok) {
+			setUploadVisible(false);
 			return alert('Could not save the listing.');
 		}
-		alert('Success');
+
+		resetForm();
 	}
 
 	return (
 		<Screen style={styles.container}>
+			<UploadScreen
+				onDone={() => setUploadVisible(false)}
+				progress={progress}
+				visible={uploadVisible}
+			/>
 			<Form
 				initialValues={{
 					title: "",
