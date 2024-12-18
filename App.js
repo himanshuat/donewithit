2d26bc4cbfc6f0846f955068aac3eed9d24cdf96
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-18 15:39:01 +0530

    Show offline notice

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 
-import AuthNavigator from './app/navigation/AuthNavigator';
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
+import OfflineNotice from './app/components/OfflineNotice';
 
 export default function App() {
 	return (
 		<GestureHandlerRootView>
+			<OfflineNotice />
 			<NavigationContainer theme={navigationTheme}>
 				<AppNavigator />
 			</NavigationContainer>
