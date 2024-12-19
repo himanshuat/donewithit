19d5d310584bb01b967d4fc91f6ba9de0208c9d1
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-19 22:34:59 +0530

    Implement Login

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
 import OfflineNotice from './app/components/OfflineNotice';
+import AuthNavigator from './app/navigation/AuthNavigator';
+import AuthContext from './app/auth/context';
 
 export default function App() {
+	const [user, setUser] = useState();
+
 	return (
 		<GestureHandlerRootView>
-			<OfflineNotice />
-			<NavigationContainer theme={navigationTheme}>
-				<AppNavigator />
-			</NavigationContainer>
+			<AuthContext.Provider value={{ user, setUser }}>
+				<OfflineNotice />
+				<NavigationContainer theme={navigationTheme}>
+					{user ? <AppNavigator /> : <AuthNavigator />}
+				</NavigationContainer>
+			</AuthContext.Provider>
 		</GestureHandlerRootView>
 	);
 }
