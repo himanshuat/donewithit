67784397f8b1e741031c1fc9238483f0d7f5c532
Himanshu Tegyalwar <himanshureddy0846@gmail.com>
2024-12-21 00:08:33 +0530

    Add overlay to activity indicator

--- app/components/ActivityIndicator.js
+++ app/components/ActivityIndicator.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import LottieView from 'lottie-react-native';
+import React from "react";
+import LottieView from "lottie-react-native";
+import { View, StyleSheet } from "react-native";
 
 function ActivityIndicator({ visible = false }) {
-    if (!visible) return null;
+	if (!visible) return null;
 
-    return <LottieView style={{ flex: 1 }} autoPlay loop source={require('../assets/animations/loader.json')} />
+	return (
+		<View style={styles.overlay}>
+			<LottieView
+				style={styles.animation}
+				autoPlay
+				loop
+				source={require("../assets/animations/loader.json")}
+			/>
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	animation: {
+		flex: 1,
+	},
+	overlay: {
+		position: "absolute",
+		backgroundColor: "white",
+		height: "100%",
+		opacity: 0.8,
+		width: "100%",
+		zIndex: 1,
+	},
+});
 
 export default ActivityIndicator;
